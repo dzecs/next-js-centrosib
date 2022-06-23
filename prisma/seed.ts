96cb6c6c88f176fdc7c4d8ea029582e12fd59b1e
Diego Ezequiel <diego.scardulla@outlook.com>
2022-06-23 09:45:42 -0300

    seeds para probar base de datos

---
+++ prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+async function main() {
+  const specializations = await prisma.specializaton.createMany({
+    data: [
+      {
+        id: 1,
+        specialization_name: "Hello",
+      },
+      {
+        id: 2,
+        specialization_name: "Ortodoncia",
+      },
+      {
+        id: 3,
+        specialization_name: "Implantes",
+      },
+      {
+        id: 4,
+        specialization_name: "Estética",
+      },
+    ],
+    skipDuplicates: true,
+  });
+
+  const doctorRober = await prisma.doctor.upsert({
+    where: { id: 1 },
+    update: {},
+    create: {
+      id: 1,
+      first_name: "Roberto",
+      last_name: "Rodriguez",
+      professional_statement: "Un médico más",
+      image_url: "../public/vercel.svg",
+    },
+  });
+  const doctorSpecializations = await prisma.doctor_Specialization.createMany({
+    data: [
+      {
+        doctor_id: 1,
+        specialization_id: 3,
+      },
+      {
+        doctor_id: 1,
+        specialization_id: 1,
+      },
+    ],
+    skipDuplicates: false,
+  });
+  console.log(doctorSpecializations.count);
+}
+
+main()
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
