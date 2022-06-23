5803462d5cb5029514973b0de208fd466104348f
Diego Ezequiel <diego.scardulla@outlook.com>
2022-06-23 20:03:59 -0300

    hola bbtos

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,9 +4,10 @@ import { useSession, signIn, signOut } from "next-auth/react";
 
 const Home: NextPage = () => {
   return (
-    <>
+    <main className="w-screen">
       <Navbar />
-    </>
+      <h1>HOLA BBTOS</h1>
+    </main>
   );
 };
 
