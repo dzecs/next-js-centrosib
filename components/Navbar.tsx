6f0442ec6c58596051ba2aac0d02e2fc9c59ffaf
Diego Ezequiel <diego.scardulla@outlook.com>
2022-06-26 14:47:56 -0300

    Agregado el login con estilo / no funcional

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,15 +3,10 @@ import Link from "next/link";
 export const Navbar = (props: {}) => {
   return (
     <nav className="">
-      <ul className="flex justify-end gap-3 pr-7 bg-blue-100 items-center h-16">
+      <ul className="flex justify-end gap-3 pr-7 bg-white items-center h-16">
         <li className="">
           <Link href="/">
             <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/signup">
-            <a>Registro</a>
           </Link>
         </li>
         <li>
@@ -22,6 +17,17 @@ export const Navbar = (props: {}) => {
         <li>
           <Link href="/contacto">
             <a>Contacto</a>
+          </Link>
+        </li>
+        <li className="">
+          <Link href="/login">
+            <a>Iniciar Sesión</a>
+          </Link>
+        </li>
+        /
+        <li>
+          <Link href="/signup">
+            <a>Registro</a>
           </Link>
         </li>
       </ul>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
 import type { NextPage } from "next";
-import { Navbar } from "../components/Navbar";
-import { useSession, signIn, signOut } from "next-auth/react";
 
 const Home: NextPage = () => {
   return <main></main>;
