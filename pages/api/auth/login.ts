7530ab5a826aeb63771c6e94571982fb8f95732f
Diego Ezequiel <diego.scardulla@outlook.com>
2022-06-29 16:57:56 -0300

    temp signup

--- pages/api/auth/login.ts
+++ pages/api/auth/login.ts
@@ -5,37 +5,37 @@ import { sign } from "jsonwebtoken";
 
 async function signup(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    try {
-      const { username, password } = req.body;
-      const existingUser = await prisma.user.findFirst({
-        where: {
-          username,
-        },
-      });
-      if (!username || !password) {
-        res.status(400);
-        throw new Error("Invalid login.");
-      }
-      if (!existingUser) {
-        res.status(403);
-        throw new Error("Invalid login.");
-      }
-
-      const validPassword = await compare(password, existingUser.password);
-      if (!validPassword) {
-        res.status(403);
-        throw new Error("Invalid login.");
-      }
-      const payload = {
-        id: existingUser.id,
-        username: existingUser.username,
-        email: existingUser.email,
-      };
-      const jwt = sign(payload, "ndeaa", { expiresIn: "30m" });
-      res.json({ payload, authToken: jwt });
-    } catch (error) {
-      console.error(error);
-    }
+    // try {
+    //   const { username, password } = req.body;
+    //   const existingUser = await prisma.user.findFirst({
+    //     where: {
+    //       username: true,
+    //     },
+    //   });
+    //   if (!username || !password) {
+    //     res.status(400);
+    //     throw new Error("Invalid login.");
+    //   }
+    //   if (!existingUser) {
+    //     res.status(403);
+    //     throw new Error("Invalid login.");
+    //   }
+    //
+    //   const validPassword = await compare(password, existingUser.password);
+    //   if (!validPassword) {
+    //     res.status(403);
+    //     throw new Error("Invalid login.");
+    //   }
+    //   const payload = {
+    //     id: existingUser.id,
+    //     username: existingUser.username,
+    //     email: existingUser.email,
+    //   };
+    //   const jwt = sign(payload, "ndeaa", { expiresIn: "30m" });
+    //   res.json({ payload, authToken: jwt });
+    // } catch (error) {
+    //   console.error(error);
+    // }
   }
 }
 
