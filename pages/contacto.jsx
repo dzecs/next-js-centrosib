86cbcc51ff2292955cc27f83b2d28de7d745d918
Diego Ezequiel <67925799+dzecs@users.noreply.github.com>
2022-06-30 18:57:47 -0300

    Update contacto.jsx

--- pages/contacto.jsx
+++ pages/contacto.jsx
@@ -46,7 +46,7 @@ export default function contacto() {
           </h2>
           <h2>
             <strong>
-              <a href="centrocib@gmail.com" class="email-contacto">
+              <a href="centrocib@gmail.com" className="email-contacto">
                 centrocib@gmail.com
               </a>
             </strong>
@@ -63,7 +63,7 @@ export default function contacto() {
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3317.6914545667732!2d-61.972224555328275!3d-33.74279380654516!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95c8649fbe5239df%3A0x31676de26d740cdf!2sInstituto%20de%20Educaci%C3%B3n%20Superior%20nro.%207%20(ex%20Normal)!5e0!3m2!1ses-419!2sar!4v1654873509381!5m2!1ses-419!2sar"
             width="76%"
             height="369px"
-            allowfullscreen=""
+            allowFullScreen=""
             loading="lazy"
             referrerpolicy="no-referrer-when-downgrade"
           ></iframe>
